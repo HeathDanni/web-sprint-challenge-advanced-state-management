da35add37f60b9344701e6e301237ea332a04a31
Heather Beckman <heatherbeckman85@gmail.com>
2020-10-25 13:38:37 -0600

    got data to render

--- smurfs/src/components/smurfData.js
+++ smurfs/src/components/smurfData.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { connect } from 'react-redux';
+import { getSmurf } from './actions/smurfActions';
 
 const SmurfData = (props) => {
+    useEffect(() => {
+        props.getSmurf()
+    }, [props.getSmurf]);
+
     return (
-        <p>this is going to be the smurf data</p>
-    );
+        <>
+        <p>name: {props.name}</p>
+        </>
+    )
 };
 
 const mapStateToProps = (state) => {
+    return {
+        name: state.name,
+        age: state.age,
+        height: state.height
+    }
 }
 
-export default SmurfData;
+export default connect(mapStateToProps, {getSmurf})(SmurfData);
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,8 +5,9 @@ import App from "./components/App";
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { smurfReducers } from './reducers/smurfReducers';
+import thunk from 'redux-thunk';
 
-let store = createStore(smurfReducers);
+let store = createStore(smurfReducers, applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}>
     <App />
