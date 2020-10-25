d878ab8ef0a9b42a95a0d2195086dd2a65fa9cab
Heather Beckman <heatherbeckman85@gmail.com>
2020-10-25 12:24:07 -0600

    created and provided store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let store = createStore(smurfReducer);
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+    </Provider>, 
+    document.getElementById("root"));
---
+++ smurfs/src/reducers/smurfReducers.js
@@ -0,0 +1,10 @@
+const initialState = {
+    
+};
+
+const smurfReducers = (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+            state,
+    };
+};
