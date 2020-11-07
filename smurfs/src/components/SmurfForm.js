fac29d30ad4ba2f020aa9c8926f4c471b2ed1d4c
Heather Beckman <heatherbeckman85@gmail.com>
2020-11-06 18:51:38 -0700

    built functionality of form inside form component

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,15 +1,17 @@
+import axios from 'axios';
 import React, {useState} from 'react';
+
 
 //form to add new smurf
 //this needs to add new smurf to smurfs array state
+const initialState = {
+    name: "",
+    age: null,
+    height: ""
+};
 
 const SmurfForm = () => {
-    const [form, setForm] = useState({
-        name: "",
-        age: 0,
-        height: "",
-        id: Date.now()
-    })
+    const [form, setForm] = useState(initialState)
 
     const handleChange = (e) => {
         e.persist();
@@ -20,33 +22,53 @@ const SmurfForm = () => {
     }
     console.log('form:', form)
 
+    const addSmurf = (e) => {
+        axios
+            .post('http://localhost:3333/smurfs', form)
+            .then((res) => {
+                console.log('post res:', res)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+        setForm(initialState);
+
+    }
+
     return (
         <div>
             <h2>Add a new smurf here:</h2>
-            <label htmlFor='name'>
-                Name:
-                <input
-                id='name'
-                name='name'
-                onChange={handleChange}
-                ></input>
-            </label>
-            <label htmlFor='age'>
-                Age:
-                <input
-                id='age'
-                name='age'
-                onChange={handleChange}
-                ></input>
-            </label>
-            <label htmlFor='height'>
-                Height:
-                <input
-                id='height'
-                name='height'
-                onChange={handleChange}
-                ></input>
-            </label>
+            <form onSubmit={addSmurf}>
+                <label htmlFor='name'>
+                    Name:
+                    <input
+                    id='name'
+                    name='name'
+                    onChange={handleChange}
+                    value={form.name}
+                    ></input>
+                </label>
+                <label htmlFor='age'>
+                    Age:
+                    <input
+                    id='age'
+                    name='age'
+                    onChange={handleChange}
+                    value={form.age}
+                    ></input>
+                </label>
+                <label htmlFor='height'>
+                    Height:
+                    <input
+                    id='height'
+                    name='height'
+                    onChange={handleChange}
+                    value={form.height}
+                    ></input>
+                </label>
+                <button type='submit'>Add</button>
+            </form>
         </div>
     )
 }
--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -3,32 +3,11 @@ import Smurf from './Smurf';
 import {connect} from 'react-redux';
 import { getSmurfs } from '../actions';
 
-//this component will loop through the state array to add data to the Smurf component
-
-// let smurfs = [
-//     {
-//       name: 'Brainey',
-//       age: 200,
-//       height: '5cm',
-//       id: 0
-//     },
-//     {
-//         name: 'Heather',
-//         age: 100,
-//         height: '6cm',
-//         id: 1
-//       }
-
-//   ];
-
-
 const SmurfList = (props) => {
     
     useEffect(() => {
         props.getSmurfs()
-    }, [])
-
-    // console.log(props.smurfs)
+    }, [props.getSmurfs])
 
     return (
         <div>
